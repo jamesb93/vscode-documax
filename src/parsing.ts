6b7c16a11b5a862777c664c866974ca04e30d975
James Bradbury <github@jamesbradbury.net>
2023-08-19 12:15:05 +0200

    simplify parsing types

--- src/parsing.ts
+++ src/parsing.ts
@@ -1,30 +1,21 @@
 // @ts-nocheck
-import Mustache from 'mustache';
-import { max } from './templates/max';
-
-function insertDefaultData(source, key) {
-    let temporary  = [];
+function insertDefaultData(source: Array<string>, key: string) {
+    let temporary: Array<string>  = [];
     
 	Object.entries(source).forEach(([k, v]) => {
-        let temp = v;
-        temp[key] = k;
-        temporary.push(temp);
+        v[key] = k;
+        temporary.push(v);
     });
     
 	return temporary;
 }
 
 
-export const sanitise = (data) => {
+export const sanitise = (data: Object) => {
     data.messages = insertDefaultData(data.messages, 'name');
 	data.attributes = insertDefaultData(data.attributes, 'name');
 	data.arguments = insertDefaultData(data.arguments, 'name');
 	data.inlets = insertDefaultData(data.inlets, 'id');
 	data.outlets = insertDefaultData(data.outlets, 'id');
     return data;
-};
-
-
-export const render = (json) => {
-	return Mustache.render(max, json);
 };
