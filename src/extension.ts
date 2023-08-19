2de25e846d9dd81909904c838708eace9e8bcea9
James Bradbury <github@jamesbradbury.net>
2023-08-19 13:46:47 +0200

    refactor file watching into a class

--- src/extension.ts
+++ src/extension.ts
@@ -1,44 +1,17 @@
 import * as vscode from 'vscode';
 import { processAndSaveAllFiles } from './file-processing';
 import { previewFile } from './file-preview';
+import { FileWatcher } from './file-watching';
 
-let fileWatcher: vscode.FileSystemWatcher | null = null;
-let isFileWatcherActive = false;
-let statusBar: vscode.StatusBarItem;
-
-function updateStatusBar() {
-    statusBar.text = `File Watcher: ${isFileWatcherActive ? 'On' : 'Off'}`;
-    statusBar.command = 'vscode-documax.toggleWatching';
-    statusBar.show();
-}
-
-function toggleFileWatching() {
-    if (fileWatcher) {
-        fileWatcher.dispose();
-        fileWatcher = null;
-        isFileWatcherActive = false;
-    } else {
-        const workspaceFolders = vscode.workspace.workspaceFolders;
-        if (workspaceFolders) {
-            fileWatcher = vscode.workspace.createFileSystemWatcher('**/*.yaml');
-            fileWatcher.onDidChange(() => {
-                if (isFileWatcherActive) {
-                    processAndSaveAllFiles();
-                }
-            });
-            isFileWatcherActive = true;
-        }
-    }
-
-    updateStatusBar();
-}
 
 export function activate(context: vscode.ExtensionContext) {
-    statusBar = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left);
-    updateStatusBar();
+    const fileWatcher = new FileWatcher();
+    fileWatcher.statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left);
+    fileWatcher.updateStatusBar();
+
     const previewDocumentCommand    = vscode.commands.registerCommand('vscode-documax.previewDoc', previewFile);
     const generateDocumentCommand   = vscode.commands.registerCommand('vscode-documax.generateDoc', processAndSaveAllFiles);
-    const toggleFileWatchingCommand = vscode.commands.registerCommand('vscode-documax.toggleWatching', toggleFileWatching);
+    const toggleFileWatchingCommand = vscode.commands.registerCommand('vscode-documax.toggleWatching', fileWatcher.toggleFileWatching);
 
     context.subscriptions.push(previewDocumentCommand);
     context.subscriptions.push(generateDocumentCommand);
--- src/file-watching.ts
+++ src/file-watching.ts
@@ -1,2 +1,48 @@
 import * as vscode from 'vscode';
 import { processAndSaveAllFiles } from './file-processing';
+
+
+export class FileWatcher {
+    fileWatcher: vscode.FileSystemWatcher | null;
+    isFileWatcherActive: boolean;
+    statusBarItem: vscode.StatusBarItem | null;
+    constructor() {
+        this.fileWatcher = null;
+        this.isFileWatcherActive = false;
+        this.statusBarItem = null;
+    }
+
+    updateStatusBar = () => {
+        if (this.statusBarItem) {
+            this.statusBarItem.text = `File Watcher: ${this.isFileWatcherActive ? 'On' : 'Off'}`;
+            this.statusBarItem.command = 'vscode-documax.toggleWatching';
+            this.statusBarItem.show();
+        }
+    }
+
+    toggleFileWatching = () => {
+        console.log(this.statusBarItem);
+        if (this.fileWatcher) {
+            this.fileWatcher.dispose();
+            this.fileWatcher = null;
+            this.isFileWatcherActive = false;
+        } else {
+            const workspaceFolders = vscode.workspace.workspaceFolders;
+            if (workspaceFolders) {
+                this.fileWatcher = vscode.workspace.createFileSystemWatcher('**/*.yaml');
+                processAndSaveAllFiles();
+                this.fileWatcher.onDidChange(() => {
+                    if (this.isFileWatcherActive) {
+                        processAndSaveAllFiles();
+                    }
+                });
+                this.isFileWatcherActive = true;
+            }
+        }
+    
+        this.updateStatusBar();
+    }
+}
+
+
+
