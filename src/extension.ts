7ab068e5266c1dfcbf5da1e1427b6839c229088b
James Bradbury <github@jamesbradbury.net>
2023-08-20 21:41:36 +0200

    add plumbing for extensions to read and use configuration

--- src/extension.ts
+++ src/extension.ts
@@ -2,12 +2,41 @@ import * as vscode from 'vscode';
 import { processAndSaveAllFiles } from './file-processing';
 import { previewFile } from './file-preview';
 import { FileWatcher } from './file-watching';
+import { checkFileExistsInWorkspaceRoot } from './common';
+import { readConfigurationFile } from './configuration';
+
+let config = {
+    
+}
+
+const configChange = () => {
+    console.log('config change');
+};
+
+const configCreate = () => {
+    console.log('config create')
+};
+
+const configDelete = () => {
+    console.log('config delete')
+};
 
 
 export function activate(context: vscode.ExtensionContext) {
     const fileWatcher = new FileWatcher();
     fileWatcher.statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left);
     fileWatcher.updateStatusBar();
+
+    const configExists = checkFileExistsInWorkspaceRoot('documax.config.json');
+    if (configExists) {
+        config = readConfigurationFile();
+    }
+
+    console.log(config);
+    const configFileWatcher = vscode.workspace.createFileSystemWatcher('**/documax.config.json');
+    configFileWatcher.onDidChange(configChange);
+    configFileWatcher.onDidCreate(configCreate);
+    configFileWatcher.onDidDelete(configDelete);
 
     const previewDocumentCommand    = vscode.commands.registerCommand('vscode-documax.previewDoc', previewFile);
     const generateDocumentCommand   = vscode.commands.registerCommand('vscode-documax.generateDoc', processAndSaveAllFiles);
