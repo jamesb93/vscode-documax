96c4f0fd0f69ded527b09f19a64b1d131062a1b6
James Bradbury <github@jamesbradbury.net>
2023-08-20 22:28:02 +0200

    delete references to now singletone services

--- src/extension.ts
+++ src/extension.ts
@@ -1,46 +1,13 @@
 import * as vscode from 'vscode';
 import { processAndSaveAllFiles } from './file-processing';
 import { previewFile } from './file-preview';
-import { FileWatcher } from './file-watching';
-import { checkFileExistsInWorkspaceRoot } from './common';
-import { readConfigurationFile } from './configuration';
-
-let config = {
-    
-}
-
-const configChange = () => {
-    console.log('config change');
-};
-
-const configCreate = () => {
-    console.log('config create')
-};
-
-const configDelete = () => {
-    console.log('config delete')
-};
-
+import { yamlFileWatcher } from './file-watching';
 
 export function activate(context: vscode.ExtensionContext) {
-    const fileWatcher = new FileWatcher();
-    fileWatcher.statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left);
-    fileWatcher.updateStatusBar();
-
-    const configExists = checkFileExistsInWorkspaceRoot('documax.config.json');
-    if (configExists) {
-        config = readConfigurationFile();
-    }
-
-    console.log(config);
-    const configFileWatcher = vscode.workspace.createFileSystemWatcher('**/documax.config.json');
-    configFileWatcher.onDidChange(configChange);
-    configFileWatcher.onDidCreate(configCreate);
-    configFileWatcher.onDidDelete(configDelete);
-
-    const previewDocumentCommand    = vscode.commands.registerCommand('vscode-documax.previewDoc', previewFile);
-    const generateDocumentCommand   = vscode.commands.registerCommand('vscode-documax.generateDoc', processAndSaveAllFiles);
-    const toggleFileWatchingCommand = vscode.commands.registerCommand('vscode-documax.toggleWatching', fileWatcher.toggleFileWatching);
+    const previewDocumentCommand        = vscode.commands.registerCommand('vscode-documax.previewDoc', previewFile);
+    const generateDocumentCommand       = vscode.commands.registerCommand('vscode-documax.generateDoc', processAndSaveAllFiles);
+    const toggleFileWatchingCommand     = vscode.commands.registerCommand('vscode-documax.toggleWatching', yamlFileWatcher.toggleFileWatching);
+    // TODO: const generateAllDocumentsCommand   = vscode.commands.registerCommand('vscode-documax.generateAllDoc', processAndSaveAllFiles);
 
     context.subscriptions.push(previewDocumentCommand);
     context.subscriptions.push(generateDocumentCommand);
