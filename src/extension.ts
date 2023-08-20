0306d7ccf6b639d936096e9b61a38f0f4bd9e2ef
James Bradbury <github@jamesbradbury.net>
2023-08-20 23:58:22 +0200

    update generation command

--- src/extension.ts
+++ src/extension.ts
@@ -1,13 +1,12 @@
 import * as vscode from 'vscode';
-import { processAndSaveAllFiles } from './file-processing';
+import { processFilesWithConfiguration } from './file-processing';
 import { previewFile } from './file-preview';
 import { yamlFileWatcher } from './file-watching';
 
 export function activate(context: vscode.ExtensionContext) {
     const previewDocumentCommand        = vscode.commands.registerCommand('vscode-documax.previewDoc', previewFile);
-    const generateDocumentCommand       = vscode.commands.registerCommand('vscode-documax.generateDoc', processAndSaveAllFiles);
+    const generateDocumentCommand       = vscode.commands.registerCommand('vscode-documax.generateDoc', processFilesWithConfiguration);
     const toggleFileWatchingCommand     = vscode.commands.registerCommand('vscode-documax.toggleWatching', yamlFileWatcher.toggleFileWatching);
-    // TODO: const generateAllDocumentsCommand   = vscode.commands.registerCommand('vscode-documax.generateAllDoc', processAndSaveAllFiles);
 
     context.subscriptions.push(previewDocumentCommand);
     context.subscriptions.push(generateDocumentCommand);
