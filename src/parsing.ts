8f0c39e22a312687e947415f65d29a8b9f226157
James Bradbury <github@jamesbradbury.net>
2023-10-03 21:34:47 +0200

    protect against non existence of XML content

--- src/parsing.ts
+++ src/parsing.ts
@@ -1,21 +1,29 @@
 // @ts-nocheck
 function insertDefaultData(source: Array<string>, key: string) {
     let temporary: Array<string>  = [];
-    
-	Object.entries(source).forEach(([k, v]) => {
-        v[key] = k;
-        temporary.push(v);
-    });
-    
+	for (const [k, v] of Object.entries(source)) {
+		v[key] = k;
+		temporary.push(v);
+	}
 	return temporary;
 }
 
 
 export const sanitise = (data: Object) => {
-    data.messages = insertDefaultData(data.messages, 'name');
-	data.attributes = insertDefaultData(data.attributes, 'name');
-	data.arguments = insertDefaultData(data.arguments, 'name');
-	data.inlets = insertDefaultData(data.inlets, 'id');
-	data.outlets = insertDefaultData(data.outlets, 'id');
+    if (data.hasOwnProperty('messages')) {
+        data.messages = insertDefaultData(data.messages, 'name');
+    }
+    if (data.hasOwnProperty('attributes')) {
+        data.attributes = insertDefaultData(data.attributes, 'name');
+    }
+    if (data.hasOwnProperty('arguments')) {
+        data.arguments = insertDefaultData(data.arguments, 'name');
+    }
+    if (data.hasOwnProperty('inlets')) {
+        data.inlets = insertDefaultData(data.inlets, 'id');
+    }
+    if (data.hasOwnProperty('outlets')) {
+        data.outlets = insertDefaultData(data.outlets, 'id');
+    }
     return data;
 };
