4edffdb3b1a735f94a32e66bb6e5ddee74404192
James Bradbury <github@jamesbradbury.net>
2023-10-03 21:57:27 +0200

    improve recursive replacement to leave values in tact

--- src/rendering.ts
+++ src/rendering.ts
@@ -1,27 +1,12 @@
 // @ts-ignore
 const mustache = require('Mustache');
-import { sanitise } from './parsing';
 import { parse } from 'yaml';
 import { marked } from 'marked';
+import { sanitise } from './parsing';
 import { objects, messages, attributes, links } from './markdownExtensions';
 
 
-function parseEditorContent(editorContent: string): Object {
-    const parsed = sanitise(parse(editorContent));
-    return parsed;
-}
-
-function renderParsedContent(data: Object, template: string): string {
-    try {
-        mustache.escape = (text: string) => text;
-        const rendered = mustache.render(template, data);
-        return rendered;
-    } catch(e: any) {
-        return e.message;
-    }
-}
-
-function recursiveObjectOperation(obj, callback) {
+function recursiveObjectOperation(obj: any, callback: (key: string, value: any) => any) {
     for (const key in obj) {
         if (typeof obj[key] === 'object' && obj[key] !== null) {
             // If the value is an object, recursively call the function
@@ -42,15 +27,31 @@ const renderer = {
 
 marked.use({ renderer, extensions: [ objects, messages, attributes, links ] });
 
+function parseEditorContent(editorContent: string): Object {
+    const parsed = parse(editorContent);
+    return sanitise(parsed);
+}
+
+function renderParsedContent(data: Object, template: string): string {
+    try {
+        mustache.escape = (text: string) => text;
+        const rendered = mustache.render(template, data);
+        return rendered;
+    } catch(e: any) {
+        return e.message;
+    }
+}
+
 export function parseAndRender(editorContent: string, template: string): string {
-    const parsed = parseEditorContent(editorContent);
-    const markdownKeys = ['digest', 'digest']
-    // TODO: this is where you would parse the text as markdown and convert it into HTML
-    recursiveObjectOperation(parsed, (key: string, value: any) => {
+    const markdownKeys = ['digest', 'digest'];
+
+    const parsedEditorContent = parseEditorContent(editorContent);
+    recursiveObjectOperation(parsedEditorContent, (key: string, value: any) => {
         if (markdownKeys.includes(key)) {
             return marked.parse(value, { gfm: true });
         }
-    }); 
-    const rendered = renderParsedContent(parsed, template);
+        return value;
+    });
+    const rendered = renderParsedContent(parsedEditorContent, template);
     return rendered;
 }
