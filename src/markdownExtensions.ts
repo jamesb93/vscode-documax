c4aa18df76fe5d1bea4f937e4b1fb68dfbb88f1b
James Bradbury <github@jamesbradbury.net>
2023-10-05 23:26:12 +0200

    simple markdown extension rework

--- src/markdownExtensions.ts
+++ src/markdownExtensions.ts
@@ -1,87 +1,36 @@
-export const objects = {
-    name: 'maxObject',
-    level: 'inline',
-    start(src: string) { 
-        const index = src.indexOf('$');
-        if (index !== -1) {
-            return index;
+const createParserToken = (name: string, shortDecorator: string, longDecorator: string, xmlElement: string) => {
+    const token = {
+        name: name,
+        level: 'inline',
+        start(src: string) {
+            const regex = new RegExp(`${longDecorator}|${shortDecorator}`);
+            const match = src.match(regex);
+            return match?.index;
+        },
+        tokenizer(src: string, tokens: Array<any>) {
+            const regex = new RegExp(`^(?:${longDecorator}|${shortDecorator})\\s+(\\w+(?:\\.\\w+)*)`);
+            const match = src.match(regex);
+            if (match) {
+                const token = {
+                    type: name,
+                    raw: match[0],
+                    text: match[1].trim(),
+                    tokens: []
+                };
+                this.lexer.inline(token.text, token.tokens);
+                return token;
+            }
+        },
+        renderer(token: any) {
+            return `<${xmlElement}>${token.text}</${xmlElement}>`;
         }
-    },
-    tokenizer(src: string, tokens: Array<any>) {
-        const rule = /^\$([^}]+)\$/;
-        const match = rule.exec(src);
-        if (match) {
-            const token = {
-                type: 'maxObject',
-                raw: match[0],
-                text: match[1].trim(),
-                tokens: []
-            };
-            this.lexer.inline(token.text, token.tokens);
-            return token;
-        }
-    },
-    renderer(token: any) {
-        return `<o>${token.text}</o>`;
-    }
+    };
+    return token;
 };
 
-export const messages = {
-    name: 'maxMessages',
-    level: 'inline',
-    start(src: string) { 
-        const index = src.indexOf('!');
-        if (index !== -1) {
-            return index;
-        }
-    },
-    tokenizer(src: string, tokens: Array<any>) {
-        const rule = /^!([^}]+)!/;
-        const match = rule.exec(src);
-        if (match) {
-            const token = {
-                type: 'maxMessages',
-                raw: match[0],
-                text: match[1].trim(),
-                tokens: []
-            };
-            this.lexer.inline(token.text, token.tokens);
-            return token;
-        }
-    },
-    renderer(token: any) {
-        return `<m>${token.text}</m>`;
-    }
-};
-
-export const attributes = {
-    name: 'maxAttributes',
-    level: 'inline',
-    start(src: string) { 
-        const index = src.indexOf('@');
-        if (index !== -1) {
-            return index;
-        }
-    },
-    tokenizer(src: string, tokens: Array<any>) {
-		const rule = /^@([^@]+)@/;
-		const match = rule.exec(src);
-        if (match) {
-            const token = {
-                type: 'maxAttributes',
-                raw: match[0],
-                text: match[1].trim(),
-                tokens: []
-            };
-            this.lexer.inline(token.text, token.tokens);
-            return token;
-        }
-    },
-    renderer(token: any) {
-        return `<at>${token.text}</at>`;
-    }
-};
-
+export const objects = createParserToken('maxObject', '@o', '@object', 'o');
+export const messages = createParserToken('maxMessages', '@m', '@message', 'm');
+export const attributes = createParserToken('maxAttributes', '@a', '@attribute', 'at');
 export const links = {
     name: 'maxLinks',
     level: 'inline',
@@ -92,24 +41,24 @@ export const links = {
         }
     },
     tokenizer(src: string, tokens: Array<any>) {
-		const rule = /^\{([^}]+)\}\(([^)]+)\)/;
-		const match = rule.exec(src);
-		if (match) {
+        const rule = /^\{([^}]+)\}\(([^)]+)\)/;
+        const match = rule.exec(src);
+        if (match) {
             const token = {
                 type: 'maxLinks',
                 raw: match[0],
                 text: match[1].trim(),
                 tokens: [{
-					url: match[2].trim(),
-					text: match[1].trim()
-				}]
+                    url: match[2].trim(),
+                    text: match[1].trim()
+                }]
             };
             this.lexer.inline(token.text, token.tokens);
             return token;
         }
     },
     renderer(token: any) {
-		const linkData = token.tokens[0];
-		return `<openfilelink filename=${linkData.url}>${linkData.text}</openfilelink>`;
+        const linkData = token.tokens[0];
+        return `<openfilelink filename=${linkData.url}>${linkData.text}</openfilelink>`;
     }
 };
